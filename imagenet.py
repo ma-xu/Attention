ff4f7b43a36bf6d009d9b42cc463542f1aaab1d7
13952522076 <melody@dynamic-10-125-217-219.dynamic.unt.edu>
2019-05-02 12:10:42 -0500

    RandomResizedCrop

--- imagenet.py
+++ imagenet.py
@@ -52,7 +52,7 @@ testdir = os.path.join(args.datapath, 'val')
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
 transform_train = transforms.Compose([
-    transforms.RandomCrop(224),
+    transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     normalize,
